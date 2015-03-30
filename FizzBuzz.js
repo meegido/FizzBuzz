56c684cf977c20be9c9934bfcb5cad9ec59ae544
Mercedes Egido <meegido@gmail.com>
2015-03-30 12:34:39 +0200

    deleted for iteration and toString test

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,33 +1,30 @@
 function FizzBuzz(number){
-	for(var i = 1; i <= 100; i++){
 
-		var isFizz = number % 3 == 0;
-		var isBuzz = number % 5 == 0;
+	var isFizz = number % 3 == 0;
+	var isBuzz = number % 5 == 0;
 
-		if(isFizz && isBuzz){ return "FizzBuzz"; }
+	if(isFizz && isBuzz){ return "FizzBuzz"; }
 
-		if(isFizz){ return "Fizz"; }
+	if(isFizz){ return "Fizz"; }
 
-		if(isBuzz){ return "Buzz"; }
+	if(isBuzz){ return "Buzz"; }
 
-		return number.toString();
-
-	}
+	return number.toString();
 
 }
 
 
-console.log(FizzBuzz(1) == 1);
-console.log(FizzBuzz(2) == 2);
-console.log(FizzBuzz(3) == "Fizz");
-console.log(FizzBuzz(4) == 4);
-console.log(FizzBuzz(5) == "Buzz");
+console.log(typeof FizzBuzz(1));
+console.log(FizzBuzz(2) );
+console.log(FizzBuzz(3) );
+console.log(FizzBuzz(4) == "4");
+console.log(FizzBuzz(5) );
 console.log(FizzBuzz(6) == "Fizz");
 console.log(FizzBuzz(7) == 7);
 console.log(FizzBuzz(8) == 8);
 console.log(FizzBuzz(9) == "Fizz");
 
-console.log (FizzBuzz(15) == "FizzBuzz");
+console.log (FizzBuzz(15) );
 
 
 
