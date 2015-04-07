b5dd6f9021c35d2b8ffa0c52d969e25bdd775777
Mercedes Egido <meegido@gmail.com>
2015-04-07 12:41:11 +0200

    keeping in a variable

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -2,29 +2,30 @@ function FizzBuzz(number){
 
 	var isFizz = number % 3 == 0;
 	var isBuzz = number % 5 == 0;
+	var whatIs = number.toString();
 
-	if(isFizz && isBuzz){ return "FizzBuzz"; }
+	if(isFizz){ whatIs = "Fizz"; }
 
-	if(isFizz){ return "Fizz"; }
+	if(isBuzz){ whatIs = "Buzz"; }
 
-	if(isBuzz){ return "Buzz"; }
+	if(isFizz && isBuzz){ whatIs = "FizzBuzz"; }
 
-	return number.toString();
+	return whatIs;
 
 }
 
 
-console.log(typeof FizzBuzz(1));
+console.log(FizzBuzz(1));
 console.log(FizzBuzz(2) );
 console.log(FizzBuzz(3) );
-console.log(FizzBuzz(4) == "4");
+console.log(FizzBuzz(4) );
 console.log(FizzBuzz(5) );
-console.log(FizzBuzz(6) == "Fizz");
-console.log(FizzBuzz(7) == 7);
-console.log(FizzBuzz(8) == 8);
-console.log(FizzBuzz(9) == "Fizz");
+console.log(FizzBuzz(6) );
+console.log(FizzBuzz(7) );
+console.log(FizzBuzz(8) );
+console.log(FizzBuzz(9) );
 
-console.log (FizzBuzz(15) );
+console.log (FizzBuzz(15));
 
 
 
